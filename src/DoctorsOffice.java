4b4ba3af4ffb581b157b4ffe6e17d5690de8c4ab
mimizana <mimizano@gmail.com>
2020-06-15 19:47:12 -0400

    DoctorsOffice

---
+++ src/DoctorsOffice.java
@@ -0,0 +1,26 @@
+public class DoctorsOffice
+{
+
+    public static void main(String[] args) {
+
+        String patientName = "Stacey Manson";
+        boolean hasInsurance = false;
+        double temp = 102.2;
+        int numVisits = 5;
+        String copay = "$15.00";
+        String nextVisitDate = "July 16,2020";
+        String phoneNumber = "301 000 0000";
+
+
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temperature:" + temp);
+        System.out.println("Patient copay:" +  copay);
+        System.out.println("Body temperature:" + temp);
+        System.out.println("Date of next visit:" + nextVisitDate);
+        System.out.println("Contact Number:" + phoneNumber);
+
+    }
+}
